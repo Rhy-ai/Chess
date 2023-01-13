e9bded5032efa3382677b69244bd1d8e07c77d9a
Rhy <rhymorse@gmail.com>
2023-01-13 22:34:18 +0000

    Started

---
+++ Piece.cs
@@ -0,0 +1,12 @@
+public class Piece
+{
+    int x;
+    int y;
+    int team;
+    public Piece(int x, int y, int team)
+    {
+        this.x = x;
+        this.y = y;
+        this.team = team;
+    }
+}
